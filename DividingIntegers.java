acdd5d10675b93d19f769c47686cc1de51d7d3fd
Max Ryan <maxryan12345@hotmail.com>
2015-11-18 03:51:33 +0000

    Added methods for addition, subtraction and multiplication

---
+++ DividingIntegers.java
@@ -0,0 +1,25 @@
+public class DividingIntegers {
+	
+	public static void main(String[] args) {
+	
+	}
+	//Method for addition
+	public int add(int x, int y) {
+		int result = x + y;
+		return result;
+	}
+	
+	//Method for Subtraction
+	public int subtract(int x, int y) {
+		int result = x - y:
+		return result;
+	}
+	//Method for multiplication
+	public int multiply(int x, int y) {
+		int result = x * y;
+		return result;
+		
+	
+	
+	
+}	
