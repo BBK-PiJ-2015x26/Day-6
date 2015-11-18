e74923d6d90219626b52bb3abd52ed0a5b406fac
Max Ryan <maxryan12345@hotmail.com>
2015-11-18 04:24:42 +0000

    Completed methods

--- DividingIntegers.java
+++ DividingIntegers.java
@@ -18,8 +18,19 @@ public class DividingIntegers {
 	public int multiply(int x, int y) {
 		int result = x * y;
 		return result;
-		
+	}
 	
-	
+	//Method for division
+	public double division(int x, int y) {
+		double a = (double) x;
+		double b = (double) y;
+		double result = a / b;
+		return result
+	}	
+	//Method for modulus
+	public int modulus(int x, int y) {
+		int result = x % y;
+		return result;
+	}
 	
 }	
