87508f2cb2f95647b26143517b5c4f4a47775b35
Max Ryan <maxryan12345@hotmail.com>
2015-11-18 23:33:24 +0000

    Created class, tried testing

--- ArrayCopier.java
+++ ArrayCopier.java
@@ -1,19 +1,28 @@
 public class ArrayCopier {
 	
 	public static void main(String[] args) {
-	
+		int[] srcArray1 = {1, 2, 3};
+		int[] dstArray1 = {0, 0, 0};
+		ArrayCopier arrayCopier = new ArrayCopier();
+		arrayCopier.copy(srcArray1, dstArray1);
+		System.out.println(dstArray1[0])
+		System.out.println(dstArray1[1])
+		System.out.println(dstArray1[2])
+		// Just returns memory addresses when testing?
 	}
-	
+
 	//Method to copy elements of first array to second array	
-	public int copy(int[] src, int dst[]) {
+	public int[] copy(int[] src, int[] dst) {
 		if (src.length < dst.length) {
 			for (int i = 0; i < dst.length; i++) {
-				dst[i] = 0:;
+				dst[i] = 0;
 			}
-		} else if (src.length >= dst.length) {
+		} 
+		else if (src.length >= dst.length) {
 			for (int i = 0; i < dst.length; i++) {
 				dst[i] = src[i];
 			}
 		}
+		return dst;
 	}
 }
