b78eaa4d06c12c53bae5d1d6ad838ab503f384fa
Max Ryan <maxryan12345@hotmail.com>
2015-12-09 02:16:17 +0000

    Added methods for Matrixchecker (incomplete)

---
+++ Matrixchecker.java
@@ -0,0 +1,32 @@
+public class Matrixchecker {
+	 
+	
+	public boolean isSymmetrical (int[] array) {
+		for (i = 0; i < array.length; i++) {
+			if array[i] != array[array.length - 1 - i] {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean isSymmetrical (int[] [] array) {
+		for (i=0; i < array.length; i++) {
+			for (j=0; j < array[0].length; i++) {
+				if (array[i][j] != array [j][i]) {
+					return false;
+				}
+			}
+		return true;
+	}
+	
+	public boolean isTriangular(int[][] array) {
+		for (i=0; i < array.lenght; i++) {
+			for (j=0; j < array[0].length; i++) {
+				if (array[i][j] 
+				
+		
+		
+		
+				
+	
